7ad6920c1c4e1750a3f4f991ed2e0922a3244dfd
ignite720 <ignite720@outlook.com>
2025-11-19 22:44:36 +0800

    init4

--- deletion_queue.hpp
+++ deletion_queue.hpp
@@ -1,29 +1,33 @@
 #pragma once
 
-#include <deque>
 #include <functional>
+#include <map>
 
 namespace wkstk {
     class DeletionQueue {
     public:
+        using Priority = size_t;
         using Deletor = std::function<void()>;
-
+        
+        static constexpr Priority MIN_PRIORITY = 0;
+        static constexpr Priority MAX_PRIORITY = -1;
+        
         ~DeletionQueue() {
             this->flush();
         }
 
-        void push(Deletor &&value) {
-            m_deletors.push_back(std::move(value));
+        void push(Deletor &&value, Priority priority = MIN_PRIORITY) {
+            m_deletors.emplace(priority, std::move(value));
         }
 
         void flush() {
             for (auto iter = m_deletors.rbegin(); iter != m_deletors.rend(); ++iter) {
-                (*iter)();
+                iter->second();
             }
             m_deletors.clear();
         }
 
     private:
-        std::deque<Deletor> m_deletors;
+        std::multimap<Priority, Deletor> m_deletors;
     };
 }
